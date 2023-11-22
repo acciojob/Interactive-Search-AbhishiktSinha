e99faf13d282214ff7de8fd45b808d1125fcf61e
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-22 14:13:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const button = document.querySelector(".btn");
 button.addEventListener("click", ()=>{
 	const input = document.querySelector(".input");
 	input.focus();
+	const search = document.querySelector(".search");
+	search.classList.add("active")
 })
